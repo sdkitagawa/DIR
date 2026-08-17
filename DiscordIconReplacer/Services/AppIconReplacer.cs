945569890099f0c340f61733473f2eafe74d53ce
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-17 12:47:55 +0900

    feat(icons): replace app icons in versioned application directories

--- DiscordIconReplacer/Services/AppIconReplacer.cs
+++ DiscordIconReplacer/Services/AppIconReplacer.cs
@@ -4,16 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using DiscordIconReplacer.Services;
 
 namespace DiscordIconReplacer.Services
 {
     public class AppIconReplacer : IIconReplacer
     {
+        private static readonly Regex VersionFolderPattern = new Regex(@"^app-\d+(\.\d+)+$", RegexOptions.Compiled);
+
         public void ReplaceAppIcon(string targetDir, string sourceIconPath)
         {
             var destinationPath = Path.Combine(targetDir, "app.ico");
             File.Copy(sourceIconPath, destinationPath, overwrite: true);
+
+            if (!Directory.Exists(targetDir))
+                return;
+
+            foreach (var subDir in Directory.GetDirectories(targetDir))
+            {
+                if (VersionFolderPattern.IsMatch(Path.GetFileName(subDir)))
+                    File.Copy(sourceIconPath, Path.Combine(subDir, "app.ico"), overwrite: true);
+            }
         }
     }
 }
