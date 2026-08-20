bbeb0baf0c986f551f6656cceffc92fcb6b09fb2
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:35:03 +0900

    refactor(services): modernize IIconReplacer interface syntax

--- DiscordIconReplacer/Services/IIconReplacer.cs
+++ DiscordIconReplacer/Services/IIconReplacer.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DiscordIconReplacer.Services;
 
-namespace DiscordIconReplacer.Services
+public interface IIconReplacer
 {
-    public interface IIconReplacer
-    {
-        void ReplaceAppIcon(string targetDir, string sourceIconPath);
-    }
+    void ReplaceAppIcon(string targetDir, string sourceIconPath);
 }
