e7af977135a1aba249fd4d6fa53fa3f82f7cfe5b
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:30:18 +0900

    refactor(facades): modernize ShortcutUpdateFacade and handle shortcut update failures

--- DiscordIconReplacer/Facades/ShortcutUpdateFacade.cs
+++ DiscordIconReplacer/Facades/ShortcutUpdateFacade.cs
@@ -1,26 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using DiscordIconReplacer.Models;
 using DiscordIconReplacer.Services;
 
-namespace DiscordIconReplacer.Facades
+namespace DiscordIconReplacer.Facades;
+
+public class ShortcutUpdateFacade
 {
-    public class ShortcutUpdateFacade
+    private readonly IShortcutUpdater _updater;
+
+    public ShortcutUpdateFacade(IShortcutUpdater updater)
     {
-        private readonly IShortcutUpdater updater;
+        _updater = updater;
+    }
 
-        public ShortcutUpdateFacade(IShortcutUpdater updater)
+    public void ApplyAll(List<ShortcutUpdateRequest> requests)
+    {
+        foreach (var request in requests)
         {
-            this.updater = updater;
-        }
-
-        public void ApplyAll(List<ShortcutUpdateRequest> requests)
-        {
-            foreach (var request in requests)
-                updater.UpdateIcon(request.ShortcutPath, request.IconPath);
+            try
+            {
+                _updater.UpdateIcon(request.ShortcutPath, request.IconPath);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine($"Failed to update shortcut '{request.ShortcutPath}': {ex.Message}");
+            }
         }
     }
 }
