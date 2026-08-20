6ec9f2bf43cdb0026aafe582a52bd0a9b5ad1bfd
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:33:02 +0900

    refactor(models): convert AppIconReplaceRequest to immutable record

--- DiscordIconReplacer/Models/AppIconReplaceRequest.cs
+++ DiscordIconReplacer/Models/AppIconReplaceRequest.cs
@@ -1,20 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DiscordIconReplacer.Models;
 
-namespace DiscordIconReplacer.Models
-{
-    public class AppIconReplaceRequest
-    {
-        public string TargetDir { get; }
-        public string IconName { get; }
-
-        public AppIconReplaceRequest(string targetDir, string iconName)
-        {
-            TargetDir = targetDir;
-            IconName = iconName;
-        }
-    }
-}
+public record AppIconReplaceRequest(string TargetDir, string IconName);
