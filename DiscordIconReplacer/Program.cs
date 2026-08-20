6f86d0cf7c7ad3e1b3c2588089548dbf74ce19b1
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:33:50 +0900

    refactor(startup): migrate application entry point to MainForm and modern C# syntax

--- DiscordIconReplacer/Program.cs
+++ DiscordIconReplacer/Program.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DiscordIconReplacer.Services;
 using DiscordIconReplacer.SystemServices;
 
+namespace DiscordIconReplacer;
 
-namespace DiscordIconReplacer
+internal static class Program
 {
-    internal static class Program
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DIR(new FileDialogService(), new SystemService()));
-        }
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm(new FileDialogService(), new SystemService()));
     }
 }
