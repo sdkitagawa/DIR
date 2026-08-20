f738e525b43bf01f087c44c1a3cf0d2d6f466695
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:34:49 +0900

    refactor(services): decouple file dialog interface from WinForms controls

--- DiscordIconReplacer/Services/IFileDialogService.cs
+++ DiscordIconReplacer/Services/IFileDialogService.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace DiscordIconReplacer.Services;
 
-namespace DiscordIconReplacer.Services
+public interface IFileDialogService
 {
-    public interface IFileDialogService
-    {
-        void ShowFileDialog(TextBox targetTextBox, string fileType, string extension);
-
-        void ShowFolderDialog(TextBox targetTextBox, string title);
-    }
+    string ShowFileDialog(string fileType, string extension);
+    string ShowFolderDialog(string title, string currentPath);
 }
