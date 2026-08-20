2b7b688b4adc4a927b0f3cdfe22f5a35bc1e21a6
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:37:14 +0900

    refactor(system-services): modernize SystemService and narrow Explorer restart exception handling

---
+++ DiscordIconReplacer/SystemServices/SystemService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DiscordIconReplacer.SystemServices;
+
+public class SystemService : ISystemService
+{
+    public void RestartExplorer()
+    {
+        foreach (var process in Process.GetProcessesByName("explorer"))
+        {
+            try
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        Process.Start("explorer.exe");
+    }
+}
