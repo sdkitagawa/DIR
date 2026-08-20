631e256ef9693e3982cb0fafc305088d6cc0c062
Douglas Kitagawa <36315915+sdkitagawa@users.noreply.github.com>
2026-08-20 18:20:13 +0900

    test(facade): cover resilient shortcut icon updates

---
+++ DiscordIconReplacer.Tests/Facades/ShortcutUpdateFacadeTests.cs
@@ -0,0 +1,62 @@
+using DiscordIconReplacer.Facades;
+using DiscordIconReplacer.Models;
+using DiscordIconReplacer.Services;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DiscordIconReplacer.Tests.Facades;
+
+public class ShortcutUpdateFacadeTests
+{
+    private readonly Mock<IShortcutUpdater> _updaterMock;
+    private readonly ShortcutUpdateFacade _facade;
+
+    public ShortcutUpdateFacadeTests()
+    {
+        _updaterMock = new Mock<IShortcutUpdater>();
+        _facade = new ShortcutUpdateFacade(_updaterMock.Object);
+    }
+
+    [Fact]
+    public void ApplyAll_CallsUpdaterForEachRequest()
+    {
+        var requests = new System.Collections.Generic.List<ShortcutUpdateRequest>
+        {
+            new("/shortcuts/discord.lnk", "/icons/app.ico"),
+            new("/shortcuts/ptb.lnk", "/icons/app.ico")
+        };
+
+        _facade.ApplyAll(requests);
+
+        _updaterMock.Verify(u => u.UpdateIcon("/shortcuts/discord.lnk", "/icons/app.ico"), Times.Once);
+        _updaterMock.Verify(u => u.UpdateIcon("/shortcuts/ptb.lnk", "/icons/app.ico"), Times.Once);
+    }
+
+    [Fact]
+    public void ApplyAll_UpdaterThrows_ContinuesWithRemaining()
+    {
+        _updaterMock.Setup(u => u.UpdateIcon("/shortcuts/bad.lnk", It.IsAny<string>()))
+            .Throws<System.Exception>();
+
+        var requests = new System.Collections.Generic.List<ShortcutUpdateRequest>
+        {
+            new("/shortcuts/bad.lnk", "/icons/app.ico"),
+            new("/shortcuts/good.lnk", "/icons/app.ico")
+        };
+
+        _facade.ApplyAll(requests);
+
+        _updaterMock.Verify(u => u.UpdateIcon("/shortcuts/good.lnk", "/icons/app.ico"), Times.Once);
+    }
+
+    [Fact]
+    public void ApplyAll_EmptyRequests_DoesNotCallUpdater()
+    {
+        var requests = new System.Collections.Generic.List<ShortcutUpdateRequest>();
+
+        _facade.ApplyAll(requests);
+
+        _updaterMock.Verify(u => u.UpdateIcon(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+    }
+}
